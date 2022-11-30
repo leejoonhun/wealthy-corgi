86b07bb08d6f2b90a60a3ec37fca907d84361182
Lee Joonhun <jhlee63@outlook.com>
2022-12-01 04:57:55 +0900

    Add skeleton of naver and yahoo finance module

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-from .alpha_vantage import get_crypto_price, get_ty, get_xr
+from .alpha_vantage import get_crypto_price, get_ty, get_xr, get_sector_perf
 from .gold_price_z import get_gold_price
-from .naver_finance import get_ko_stock_price
-from .yahoo_finance import get_de_stock_price, get_fr_stock_price, get_us_stock_price
+from .naver_finance import get_ko_stock_prices
+from .yahoo_finance import get_de_stock_prices, get_fr_stock_prices, get_us_stock_prices
--- utils/alpha_vantage.py
+++ utils/alpha_vantage.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import pandas_datareader.data as web
 import requests
 import yaml
@@ -6,12 +5,12 @@ import yaml
 from config import RSRC_DIR
 
 with open(RSRC_DIR / "api_keys.yml", "r") as f:
-    api_key = yaml.safe_load(f)["Alpha Vantage"]
+    API_KEY = yaml.safe_load(f)["Alpha Vantage"]
 
 
 def get_xr(quote: str, base: str = "KRW"):
     return (
-        web.DataReader(f"{quote}/{base}", "av-forex", api_key=api_key)
+        web.DataReader(f"{quote}/{base}", "av-forex", api_key=API_KEY)
         .loc["Exchange Rate"]
         .values[0]
     )
@@ -19,7 +18,7 @@ def get_xr(quote: str, base: str = "KRW"):
 
 def get_crypto_price(quote: str):
     return (
-        web.DataReader(f"{quote}/KRW", "av-forex", api_key=api_key)
+        web.DataReader(f"{quote}/KRW", "av-forex", api_key=API_KEY)
         .loc["Exchange Rate"]
         .values[0]
     )
@@ -27,6 +26,10 @@ def get_crypto_price(quote: str):
 
 def get_ty(year: int = 10):
     assert year in [2, 5, 10, 30], "Year must be 2, 5, 10, or 30"
-    url = f"https://www.alphavantage.co/query?function=TREASURY_YIELD&maturity={year}year&apikey={api_key}"
+    url = f"https://www.alphavantage.co/query?function=TREASURY_YIELD&maturity={year}year&apikey={API_KEY}"
     data = requests.get(url).json()
     return float(data["data"][0]["value"]) / 100
+
+
+def get_sector_perf():
+    return web.get_sector_performance_av(api_key=API_KEY)
